004b3ecd65f8f896c671d8c9ffff10ac4b6b634a
Khadija Noor <khadija.noor@A006-01504.local>
2024-07-08 14:26:18 +0500

    Database created

--- myBlog/myBlog/settings.py
+++ myBlog/myBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-irsn^_v$bipe52$!0nl#^@(rb5aeslkbjq-xk%-q*(_^&9*m$o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 
 # Application definition
